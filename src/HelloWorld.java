8996f12d117066a30e2d06d6de1ee2e76432e7f9
yusufkhan1 <31706103+yusufkhan1@users.noreply.github.com>
2017-09-08 17:26:20 -0500

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// Yusuf will add his name in a later commit
-		System.out.println("Maria Maxon");
+		System.out.println("Maria Maxon and Yusuf Khan");
 	}
 }
